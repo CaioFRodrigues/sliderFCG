6eaf44be4977acfc0f79a7464e6fa07e4923c939
Caio_Rodrigues <caio_f.r@hotmail.com>
2017-07-09 18:23:02 -0300

    Fixes to class Rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -133,6 +133,91 @@ struct SceneObject
     glm::vec3    bbox_max;
 };
 
+//////////// RECTANGLE //////////////////////////////////////////////////////////////////////////////
+namespace RECTANGLE{
+    class Rectangle
+    {
+        private:
+            float positionX;
+            float positionY;
+            int width;
+            int height;
+            ObjModel model=NULL;
+        public:
+            Rectangle(float positionX, float positionY, int width, int height, ObjModel model)
+            {
+                this->positionX = positionX;
+                this->positionY = positionY;
+                this->width = width;
+                this->height = height;
+                this->model = model;
+            }
+            float getPositionX();
+            float getPositionY();
+            int getWidth();
+            int getHeight();
+            ObjModel getModel();
+            void setModel(ObjModel model);
+            bool collide(Rectangle rectangle);
+            bool isInside(float positionX, float positionY);
+    };
+
+    float Rectangle::getPositionX(){
+        return this->positionX;
+    }
+
+    float Rectangle::getPositionY(){
+        return this->positionY;
+    }
+
+    int Rectangle::getWidth(){
+        return this->width;
+    }
+
+    int Rectangle::getHeight(){
+        return this->height;
+    }
+
+    ObjModel Rectangle::getModel(){
+      return this->model;
+    }
+
+    void Rectangle::setModel(ObjModel model){
+      this->model = model;
+    }
+
+    bool Rectangle::collide(Rectangle rectangleTwo){
+        float rectangleTwo_posX = rectangleTwo.getPositionX();
+        float rectangleTwo_posY = rectangleTwo.getPositionY();
+        float rectangleTwo_width = rectangleTwo.getWidth();
+        float rectangleTwo_height = rectangleTwo.getHeight();
+        float position1_X = (rectangleTwo_posX + 1);
+        float position1_Y = (rectangleTwo_posY + 1);
+        float position2_X = (rectangleTwo_posX + rectangleTwo_width - 1);
+        float position2_Y = (rectangleTwo_posY + 1);
+        float position3_X = (rectangleTwo_posX + rectangleTwo_width - 1);
+        float position3_Y = (rectangleTwo_posY + rectangleTwo_height - 1);
+        float position4_X = (rectangleTwo_posX + 1);
+        float position4_Y = (rectangleTwo_posY + rectangleTwo_height - 1);
+
+        if (this->isInside(position1_X, position1_Y) or this->isInside(position2_X, position2_Y) or this->isInside(position3_X, position3_Y) or this->isInside(position4_X, position4_Y))
+            return true;
+        return false;
+    }
+
+
+    bool Rectangle::isInside(float positionX, float positionY){
+        if (this->positionX <= positionX < this->positionX + this->width)
+                if (this->positionY <= positionY < this->positionY + this->height)
+                    return true;
+        return false;
+    }
+
+}
+
+using namespace RECTANGLE;
+
+
 // Abaixo definimos variáveis globais utilizadas em várias funções do código.
 
 // A cena virtual é uma lista de objetos nomeados, guardados em um dicionário
@@ -1580,136 +1665,12 @@ void move_car(){
 }
 
 
-//////////// RECTANGLE //////////////////////////////////////////////////////////////////////////////
-class Rectangle
-{
-    private:
-        float positionX;
-        float positionY;
-        int width;
-        int height;
-        ObjModel model=NULL;
-    public:
-        Rectangle(float positionX, float positionY, int width, int height, ObjModel model)
-        {
-            this->positionX = positionX;
-            this->positionY = positionY;
-            this->width = width;
-            this->height = height;
-            this->model = model;
-        }
-        float getPositionX();
-        float getPositionY();
-        int getWidth();
-        int getHeight();
-        ObjModel getModel();
-        void setModel(ObjModel model);
-        bool collide(Rectangle rectangle);
-        bool isInside(float positionX, float positionY);
-};
 
-float Rectangle::getPositionX(){
-    return this->positionX;
-}
-
-float Rectangle::getPositionY(){
-    return this->positionY;
-}
-
-int Rectangle::getWidth(){
-    return this->width;
-}
-
-int Rectangle::getHeight(){
-    return this->height;
-}
-
-ObjModel Rectangle::getModel(){
-  return this->model;
-}
-
-void Rectangle::setModel(ObjModel model){
-  this->model = model;
-}
-
-bool Rectangle::collide(Rectangle rectangleTwo){
-    float rectangleTwo_posX = rectangleTwo.getPositionX();
-    float rectangleTwo_posY = rectangleTwo.getPositionY();
-    float rectangleTwo_width = rectangleTwo.getWidth();
-    float rectangleTwo_height = rectangleTwo.getHeight();
-    float position1_X = (rectangleTwo_posX + 1);
-    float position1_Y = (rectangleTwo_posY + 1);
-    float position2_X = (rectangleTwo_posX + rectangleTwo_width - 1);
-    float position2_Y = (rectangleTwo_posY + 1);
-    float position3_X = (rectangleTwo_posX + rectangleTwo_width - 1);
-    float position3_Y = (rectangleTwo_posY + rectangleTwo_height - 1);
-    float position4_X = (rectangleTwo_posX + 1);
-    float position4_Y = (rectangleTwo_posY + rectangleTwo_height - 1);
-
-    if (this->isInside(position1_X, position1_Y) or this->isInside(position2_X, position2_Y) or this->isInside(position3_X, position3_Y) or this->isInside(position4_X, position4_Y))
-        return true;
-    return false;
-}
-
-bool Rectangle::isInside(float positionX, float positionY){
-    if (this->positionX <= positionX < this->positionX + this->width)
-            if (this->positionY <= positionY < this->positionY + this->height)
-                return true;
-    return false;
-}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-//////////////  MAP  ////////////////////////////////////////////////////////////////////////////////////////////
-/*
-class Map
-{
-    private:
-        int dimension_x=50;
-        int dimension_y=50;
-        int rectangle_width;
-        int rectangle_height;
-        void initializeMap();
-    public:
-        Map(int dimension_x, int dimension_y, int rectangle_width, int rectangle_height){
-            this.dimension_x = dimension_x;
-            this.dimension_y = dimension_y;
-            this.rectangle_width = rectangle_width;
-            this.rectangle_height = rectangle_height;
-        }
-}
 
-int Map::getDimension_X(){
-    return this.dimension_x;
-}
-
-int Map::getDimension_Y(){
-    return this.dimension_y;
-}
-
-int Map::getRectangle_width(){
-    return this.rectangle_width;
-}
-
-int Map::getDimension_height(){
-    return this.dimension_x;
-}
-
-void Map::initializeMap(){
-    float position_x = 0.0f;
-    float position_y = 0.0f;
-    int ID =
-    for(int i = 0; i < this.dimension_x; i++){
-        for(int j = 0; j < this.dimension_y; j++){
-            ObjModel planemodel("../../data/plane.obj");
-            ComputeNormals(&planemodel);
-            BuildTrianglesAndAddToVirtualScene(&planemodel);
-            this.rectangles[i][j] = new Rectangle(position_x, position_y, 2, 2, planemodel, );
-        }
-    }
-}
-*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
